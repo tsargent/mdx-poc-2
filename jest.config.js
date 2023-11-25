bcb32381fc148ed4bec2ade093841e344c609192
Tyler Sargent <tsargent@gmail.com>
2023-11-24 22:15:09 -0500

    add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,31 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+module.exports = {
+  // The root of your project directory
+  rootDir: ".",
 
-/** @type {import('jest').Config} */
-const config = {};
+  // A list of file extensions your modules use
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
 
-module.exports = config;
+  // Setup Babel to handle JavaScript files
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+
+  // Ignore specific modules in node_modules from Babel transformations
+  transformIgnorePatterns: [
+    "/node_modules/(?!(@mdx-js/mdx|vfile|vfile-message|unist-util-stringify-position|markdown-extensions|devlop|remark-mdx|mdast-util-mdx|mdast-util-mdx-expression|mdast-util-mdx-jsx)/)",
+  ],
+
+  // Enable ES6 modules support
+  testEnvironment: "node",
+  globals: {
+    NODE_ENV: "test",
+  },
+
+  // Optional: Configure Jest to run specific test files or folders
+  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+
+  // Optional: Set up Jest coverage reporting
+  // collectCoverage: true,
+  // collectCoverageFrom: ['src/**/*.js'],
+  // coverageReporters: ['lcov', 'text'],
+};
